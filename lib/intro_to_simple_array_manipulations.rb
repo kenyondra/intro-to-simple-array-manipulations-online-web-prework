8fff5db88829d8466dfcdef8e2fb2d895a8a89a8
Kenyondra J Conley <kenyondra_conley@yahoo.com>
2019-06-07 11:54:47 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -42,3 +42,22 @@ def using_insert(list_of_esoteric_programming_languages, another_esoteric_langua
   @new_array = @list_of_esoteric_programming_languages.insert(4, @another_esoteric_language)
 end
 
+def using_uniq(captain_planet_and_the_planeteers)
+  captain_planet_and_the_planeteers = ["Captain Planet", "Gaia", "Kwame", "Gi", "Linka", "Wheeler", "Gaia"]
+  new_array = captain_planet_and_the_planeteers.uniq 
+end
+
+def using_flatten(private_colleges_in_newyork)
+  private_colleges_in_newyork = ["New York University", ["Manhattan School of Music", "Columbia University"], "The Juilliard School", "Bard College", "Cooper Union"]
+  flat_array = private_colleges_in_newyork.flatten 
+end
+
+def using_delete(my_list, my_element)
+  my_list.delete(my_element)
+end
+
+def using_delete_at(famous_robots, integer)
+  famous_robots = ["the dog from doctor who", "R2D2", "Ultron"]
+  integer = 2 
+  deleted_robot = famous_robots.delete_at(integer)
+end
