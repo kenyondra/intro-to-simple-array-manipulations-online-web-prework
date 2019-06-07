4afab2a905a81945fa5f53065e0c19912828f607
Kenyondra J Conley <kenyondra_conley@yahoo.com>
2019-06-07 11:27:40 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -11,4 +11,4 @@ def using_unshift(neighborhoods_in_northwest_brooklyn, new_neighborhood)
 end
 
 def using_pop(great_hits_of_the_nineties)
-  @great_hits_of_the_nineties = ["Baby One More Time", ]
+  @great_hits_of_the_nineties = ["Baby One More Time", "Smells "]
